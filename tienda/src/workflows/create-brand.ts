fceb86317b6e609859f71d030beb78630fc4c26d
spjhon <spjhon@gmail.com>
2025-03-28 22:51:42 -0500

    se continua con el workflow del cms

--- tienda/src/workflows/create-brand.ts
+++ tienda/src/workflows/create-brand.ts
@@ -1,76 +1,78 @@
-
 //importaciones para crear un step
+import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
+import { BRAND_MODULE } from "../modules/brand";
+import BrandModuleService from "../modules/brand/service";
+
+//importacione para utilizar el step en un workflow
 import {
-    createStep,
-    StepResponse,
-  } from "@medusajs/framework/workflows-sdk"
-  import { BRAND_MODULE } from "../modules/brand"
-  import BrandModuleService from "../modules/brand/service"
+  // ...
+  createWorkflow,
+  WorkflowResponse,
+} from "@medusajs/framework/workflows-sdk";
 
+//este import es para crear un event desde el workflow que lo agarre
+//el subscriber y asi hacer algo asyncrono fuera del ciclo normal de medusa
+import { emitEventStep } from "@medusajs/medusa/core-flows";
 
-  //importacione para utilizar el step en un workflow
-  import {
-    // ...
-    createWorkflow,
-    WorkflowResponse,
-  } from "@medusajs/framework/workflows-sdk"
+export type CreateBrandStepInput = {
+  name: string;
+};
 
+//este es un step, no es el workflow o la aplicacion de es, es el step que utiliza servicios creados en los modulos po rmedio +
+//del modelo de datos en brand.ts dentro de models y los servicos integrados en services.
+export const createBrandStep = createStep(
+  "create-brand-step",
+  async (input: CreateBrandStepInput, { container }) => {
+    const brandModuleService: BrandModuleService =
+      container.resolve(BRAND_MODULE);
 
-  
+    //esta es la funcion de compensacion que lo que hace es revertir lo que se ha hecho si en algum momento de los spteps
+    // sale un error
+    async (id: string, { container }) => {
+      const brandModuleService: BrandModuleService =
+        container.resolve(BRAND_MODULE);
+      await brandModuleService.deleteBrands(id);
+    };
 
+    //No olvidar que el createBrands es una funcion automatica que se crea al crear el service.ts y utiliza las
+    //funciones factory de medusa.
+    const brand = await brandModuleService.createBrands(input);
 
+    console.log("A continuacion, el container:", container.registrations);
 
-
-  export type CreateBrandStepInput = {
-    name: string
+    return new StepResponse(brand, brand.id);
   }
-  
-  //este es un step, no es el workflow o la aplicacion de es, es el step que utiliza servicios creados en los modulos po rmedio +
-  //del modelo de datos en brand.ts dentro de models y los servicos integrados en services.
-  export const createBrandStep = createStep(
-    "create-brand-step",
-    async (input: CreateBrandStepInput, { container }) => {
+);
 
 
-      const brandModuleService: BrandModuleService = container.resolve(
-        BRAND_MODULE
-      )
-      
 
-      //esta es la funcion de compensacion que lo que hace es revertir lo que se ha hecho si en algum momento de los spteps
-      // sale un error
-      async (id: string, { container }) => {
-        const brandModuleService: BrandModuleService = container.resolve(
-          BRAND_MODULE
-        )
-        await brandModuleService.deleteBrands(id)
-      }
-
-      //No olvidar que el createBrands es una funcion automatica que se crea al crear el service.ts y utiliza las
-      //funciones factory de medusa.
-      const brand = await brandModuleService.createBrands(input)
-
-      console.log("A continuacion, el container:", container.registrations)
-
-      return new StepResponse(brand, brand.id)
-    }
-  )
 
 
 
 //estos son los types para el workflow
-  type CreateBrandWorkflowInput = {
-    name: string
-  }
+type CreateBrandWorkflowInput = {
+  name: string;
+};
 
-  //este es el workflow
-  export const createBrandWorkflow = createWorkflow(
-    "create-brand",
-    //esto segun la documentacion es una funcion constructora que aceta el input que es un object
-    (input: CreateBrandWorkflowInput) => {
-      const brand = createBrandStep(input)
-  
+//este es el workflow
+export const createBrandWorkflow = createWorkflow(
+  "create-brand",
+  //esto segun la documentacion es una funcion constructora que aceta el input que es un object
+  (input: CreateBrandWorkflowInput) => {
+
+
+
+    //este es un step dentro del workflow
+    const brand = createBrandStep(input);
+
+
     
-      return new WorkflowResponse(brand)
-    }
-  )
+    emitEventStep({
+      eventName: "brand.created",
+      data: {
+        id: brand.id,
+      },
+    });
+    return new WorkflowResponse(brand);
+  }
+);
