8db6f14ee55f1b39e54f2f4ed2ae13be823d2147
Juan Aristizabal <spjhon@gmail.com>
2025-04-03 17:22:08 -0500

    se hace un pequeño ensayo con la forma de llamar apis

---
+++ tienda/src/api/admin/hello-world/[category]/product/[id]/route.ts
@@ -0,0 +1,13 @@
+import type {
+    MedusaRequest,
+    MedusaResponse,
+  } from "@medusajs/framework/http"
+  
+  export const GET = async (
+    req: MedusaRequest,
+    res: MedusaResponse
+  ) => {
+    res.json({
+      message: `[GET] Hello desde category - ${req.params.category}!`
+    })
+  }
